17e2ded07e12892f5357fd403b4b9cf20f2bc285
florent <atakoraflorent@gmail.com>
2023-07-01 22:18:18 +0200

    configuration du backend firebase

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'expo-firebase-app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/storage';
 
 export default function App() {
   return (
@@ -18,3 +22,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const firebaseConfig = {
+  // Vos informations de configuration Firebase
+  apiKey: 'AIzaSyB-XWsOy0f1tZNkT6mtQ7zkDj3bXYSnsvQ',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'appreactnative-a9f79',
+  storageBucket: 'Yappreactnative-a9f79.appspot.com',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+// Vérifiez si Firebase est déjà initialisé pour éviter les erreurs
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
